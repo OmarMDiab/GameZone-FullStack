e5657ef16d09409bcadf67484d7e9371a788bc0b
M0hAyman <m7amadaymankh@gmail.com>
2023-12-29 18:32:45 +0200

    DB and controllers initial commit

---
+++ Gamezone/Gamezone/Controllers/AdminsController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Gamezone.Models;
+
+namespace Gamezone.Controllers
+{
+    public class AdminsController : Controller
+    {
+        private GamesDataEntities1 db = new GamesDataEntities1();
+
+        // GET: Admins
+        [Authorize(Roles = "Admin")] // Restrict access to Admin role
+        public ActionResult Index()
+        {
+            var admins = db.Admins.Include(a => a.User);
+            return View(admins.ToList());
+        }
+
+        // GET: Admins/Details/5
+        [Authorize(Roles = "Admin")] // Restrict access to Admin role
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Admin admin = db.Admins.Find(id);
+            if (admin == null)
+            {
+                return HttpNotFound();
+            }
+            return View(admin);
+        }
+
+        // GET: Admins/Create
+        [Authorize(Roles = "Admin")] // Restrict access to Admin role
+        public ActionResult Create()
+        {
+            ViewBag.user_id = new SelectList(db.Users, "user_id", "user_name");
+            return View();
+        }
+
+        // POST: Admins/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Admin")] // Restrict access to Admin role
+        public ActionResult Create([Bind(Include = "admin_key,user_id")] Admin admin)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Admins.Add(admin);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.user_id = new SelectList(db.Users, "user_id", "user_name", admin.user_id);
+            return View(admin);
+        }
+
+        // GET: Admins/Edit/5
+        [Authorize(Roles = "Admin")] // Restrict access to Admin role
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Admin admin = db.Admins.Find(id);
+            if (admin == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.user_id = new SelectList(db.Users, "user_id", "user_name", admin.user_id);
+            return View(admin);
+        }
+
+        // POST: Admins/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Admin")] // Restrict access to Admin role
+        public ActionResult Edit([Bind(Include = "admin_key,user_id")] Admin admin)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(admin).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.user_id = new SelectList(db.Users, "user_id", "user_name", admin.user_id);
+            return View(admin);
+        }
+
+        // GET: Admins/Delete/5
+        [Authorize(Roles = "Admin")] // Restrict access to Admin role
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Admin admin = db.Admins.Find(id);
+            if (admin == null)
+            {
+                return HttpNotFound();
+            }
+            return View(admin);
+        }
+
+        // POST: Admins/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Admin")] // Restrict access to Admin role
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Admin admin = db.Admins.Find(id);
+            db.Admins.Remove(admin);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Gamezone/Gamezone/Controllers/Game_titlesController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Gamezone.Models;
+
+namespace Gamezone.Controllers
+{
+    //[Authorize] // Require authentication for all actions
+    public class Game_titlesController : Controller
+    {
+        private GamesDataEntities1 db = new GamesDataEntities1();
+
+        // GET: Game_titles
+        public ActionResult Index()
+        {
+            var gameTitles = db.Game_titles.ToList();
+            return View(gameTitles);
+        }
+
+        // GET: Game_titles/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Game_titles game_titles = db.Game_titles.Find(id);
+            if (game_titles == null)
+            {
+                return HttpNotFound();
+            }
+            return View(game_titles);
+        }
+
+        // GET: GameTitles/Create
+        //[Authorize(Roles = "Admin")] // Only Admins can create new game titles
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+
+
+        // POST: Game_titles/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        //[Authorize(Roles = "Admin")] // Only Admins can create new game titles
+        public ActionResult Create([Bind(Include = "game_id,game_name,description,genre,platform,developer,release_date,ign_rate")] Game_titles game_titles)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Game_titles.Add(game_titles);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(game_titles);
+        }
+
+        // GET: Game_titles/Edit/5
+        //[Authorize(Roles = "Admin")] // Only Admins can edit game titles
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Game_titles game_titles = db.Game_titles.Find(id);
+            if (game_titles == null)
+            {
+                return HttpNotFound();
+            }
+            return View(game_titles);
+        }
+
+        // POST: Game_titles/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        //[Authorize(Roles = "Admin")] // Only Admins can edit game titles
+        public ActionResult Edit([Bind(Include = "game_id,game_name,description,genre,platform,developer,release_date,ign_rate")] Game_titles game_titles)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(game_titles).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(game_titles);
+        }
+
+        // GET: Game_titles/Delete/5
+        //[Authorize(Roles = "Admin")] // Only Admins can delete game titles
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Game_titles game_titles = db.Game_titles.Find(id);
+            if (game_titles == null)
+            {
+                return HttpNotFound();
+            }
+            return View(game_titles);
+        }
+
+        // POST: Game_titles/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        //[Authorize(Roles = "Admin")] // Only Admins can delete game titles
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Game_titles game_titles = db.Game_titles.Find(id);
+            db.Game_titles.Remove(game_titles);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
